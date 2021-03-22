1492d26ee273db5c77ff6ecde905144cf0ab299e
Emil H. Clausen <sequoiia@hummel.yt>
2021-03-22 16:18:06 +0100

    Added svcAcc, api-key and acl nuke script

--- scripts/nuke-svcacc-acls-apikeys.ps1
+++ scripts/nuke-svcacc-acls-apikeys.ps1
@@ -34,9 +34,65 @@ foreach ($apiKey in $responseApiKeys) {
 
 # Clear-Variable -name hashTableOfServiceAccountIds
 
+$acls = @()
+$responseAcls = (ccloud kafka acl list --cluster $($Cluster) -o json) | Out-String | ConvertFrom-Json
+foreach ($acl in $responseAcls) {
+  if ( $hashTableOfServiceAccountIds.ContainsKey($acl.service_account_id.substring(5)) -eq $true ) {
+    $acls += $acl
+  }
+}
+
+
 
 if ($DisableDryRun) {
   Write-Host "`n"
+
+  Write-Host "Acls:`n"
+  foreach($Acl in $acls) {
+    $serviceAccountId = $Acl.service_account_id.substring(5)
+    Write-Host "$($serviceAccountId) $($Acl.operation) $($Acl.permission) $($Acl.resource) $($Acl.type) $($Acl.name)"
+    $additionalArgs = ""
+
+    if ( $Acl.type -eq 'PREFIXED' )
+    {
+      $additionalArgs += "--prefix"
+    } 
+
+    Switch ($Acl.resource)
+    {
+      {$_ -match 'TOPIC'} {
+        $topicName = $Acl.name
+
+        if ( $topicName -eq "'*'" )
+        {
+          $topicName = '''""'''
+        } 
+
+        Write-Host "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation.ToLower()) --topic $($topicName) $($additionalArgs)"
+        
+        Invoke-Expression "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation.ToLower()) --topic $($topicName) $($additionalArgs)"
+      }
+      {$_ -match 'GROUP'} {
+        Invoke-Expression "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation) --consumer-group $($Acl.name) $($additionalArgs)"
+      }
+      {$_ -match 'CLUSTER'} {
+        Invoke-Expression "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation) --cluster-scope"
+      }
+    }
+  }
+
+  Write-Host "Api keys:`n"
+  foreach ($ApiKey in $apiKeys) {
+    Invoke-Expression "ccloud api-key delete $($ApiKey.key)"
+  }
+
+
+  Write-Host "Service accounts:`n"
+  foreach ($ServiceAccount in $serviceAccounts) {
+    Invoke-Expression "ccloud service-account delete $($ServiceAccount.id)"
+  }
+
+
 } else {
   Write-Host "::DryRun::`n`n"
 
@@ -63,6 +119,38 @@ if ($DisableDryRun) {
   Write-Host "Api keys:`n"
   foreach ($ApiKey in $apiKeys) {
     Write-Host "ccloud api-key delete $($ApiKey.key) --cluster $($Cluster)"
+  }
+
+  Write-Host "Acls:`n"
+  foreach($Acl in $acls) {
+    $serviceAccountId = $Acl.service_account_id.substring(5)
+    Write-Host "$($serviceAccountId) $($Acl.operation) $($Acl.permission) $($Acl.resource) $($Acl.type) $($Acl.name)"
+    $additionalArgs = ""
+
+    if ( $Acl.type -eq 'PREFIXED' )
+    {
+      $additionalArgs += "--prefix"
+    }
+
+    Switch ($Acl.resource)
+    {
+      {$_ -match 'TOPIC'} {
+        $topicName = $Acl.name
+
+        if ( $topicName -eq "'*'" )
+        {
+          $topicName = '''""'''
+        } 
+
+        Write-Host "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation.ToLower()) --topic $($topicName) $($additionalArgs)"
+      }
+      {$_ -match 'GROUP'} {
+        Write-Host "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation) --consumer-group $($Acl.name) $($additionalArgs)"
+      }
+      {$_ -match 'CLUSTER'} {
+        Write-Host "ccloud kafka acl delete --cluster $($Cluster) --$($Acl.permission.ToLower()) --service-account $($serviceAccountId) --operation $($Acl.operation) --cluster-scope"
+      }
+    }
   }
 
 }
