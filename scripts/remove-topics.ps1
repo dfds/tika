1cd01a1582f4b464ff91c299bca91a6b5efb59e3
Emil H. Clausen <sequoiia@hummel.yt>
2021-05-19 11:43:37 +0200

    Updated remove-topics.ps1 with multi cluster support

--- scripts/remove-topics.ps1
+++ scripts/remove-topics.ps1
@@ -2,37 +2,29 @@
 
 param(    
   [Parameter(Mandatory = $False, Position = 0, ValueFromPipeline = $false)]
-  [string[]]
-  $Topics = "",
+  [string]
+  $TopicsPath = "",
 
   [Parameter(Mandatory = $False, Position = 1, ValueFromPipeline = $false)]
-  [string]
-  $Cluster = "",
-
-  [Parameter(Mandatory = $False, Position = 2, ValueFromPipeline = $false)]
   [switch]
   $DisableDryRun = $false
 )
+
+$Topics = Get-Content $TopicsPath | Out-String | ConvertFrom-Csv
+
 
 if ($DisableDryRun) {
 
   Write-Host "Topics:`n"
   foreach ($Topic in $topics) {
     Write-Host $Topic
-    Invoke-Expression "ccloud kafka topic delete $($Topic) --cluster $($Cluster)"
+    Invoke-Expression "ccloud kafka topic delete $($Topic.name) --cluster $($Topic.clusterId) --environment $($Topic.environmentId)"
   }
   
   Write-Host "`n"
 
 } else {
-  Write-Host "::DryRun::`n`n"
-
-  Write-Host "Topics to be deleted:`n"
-  foreach ($Topic in $Topics) {
-    Write-Host $Topic
-    Write-Host "`n"
-  }
-  
+  Write-Host "::DryRun::`n`n"  
 
   # Commands that will be run if this is the case
 
@@ -40,7 +32,7 @@ if ($DisableDryRun) {
 
   Write-Host "Topics:`n"
   foreach ($Topic in $Topics) {
-    Write-Host "ccloud kafka topic delete $($Topic) --cluster $($Cluster)"
+    Write-Host "ccloud kafka topic delete $($Topic.name) --cluster $($Topic.clusterId) --environment $($Topic.environmentId)"
   }
 
 }
