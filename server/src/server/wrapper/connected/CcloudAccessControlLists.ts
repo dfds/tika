45029806ca01361d1c22854c2a0c08132b73f4ad
Willi Carlsen <carlsenwilli@gmail.com>
2022-04-28 14:51:25 +0200

    Alter acls to utilize json output

--- server/src/server/wrapper/connected/CcloudAccessControlLists.ts
+++ server/src/server/wrapper/connected/CcloudAccessControlLists.ts
@@ -1,19 +1,35 @@
 import { parse } from "./../parser";
 import { executeCli } from "./executeCli";
 import { GetConfig } from "../../config";
+import { Deserializer, ConcatOutput } from "../utils";
+import { ListAcls } from "../model/acls";
 
 export class CcloudAccessControlLists implements AccessControlLists {
     async getAccessControlLists(): Promise<AccessControlList[]> {
         let config = GetConfig();
-        let result = await executeCli(["kafka", "acl", "list", "--cluster", config.clusterId, "--environment", config.environmentId]);
-        let resultObjects = parse(result) as AccessControlList[];
+        let result = await executeCli(["kafka", "acl", "list", "--cluster", config.clusterId, "--environment", config.environmentId, "--output", "json"]);
 
-        resultObjects.forEach(elem => {
-            elem.UserId = elem.UserId.split(':')[1];
-        });
-
-        return resultObjects;
-    }
+        let combinedResult = ConcatOutput(result);
+        let deserializedResult : ListAcls;
+        try {
+            deserializedResult = Deserializer<ListAcls>(combinedResult);
+        } catch (error) {
+            return error;
+        }
+  
+        return deserializedResult.map(t => {
+          let obj = {
+            UserId: "", // removed property by Confluent
+            ServiceAccountId: t.principal.split(":")[1],
+            Permission: t.permission,
+            Operation: t.operation,
+            Resource: t.resource_type,
+            Name: t.resource_name,
+            Type: t.pattern_type
+          };
+          return obj;
+        })      
+      }
 
     async createAccessControlList(
         serviceAccountId: number,
